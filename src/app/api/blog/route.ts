94b95425975b5e25a41d65dfa700e0e57e41f274
Miradil <miradild404@gmail.com>
2023-09-12 12:06:20 +0900

    add get and post method

--- src/app/api/blog/route.ts
+++ src/app/api/blog/route.ts
@@ -11,11 +11,27 @@ export async function main(){
     
 }
 
+//GET
 export const GET = async (req: Request, res: NextRequest) => {
     try {
         await main();
         const posts = await prisma.post.findMany();
         return NextResponse.json({maessage: "success", posts}, {status: 200});
+    } catch (error) {
+        return NextResponse.json({maessage: "Error", error}, {status: 500});
+    }
+    finally {
+        await prisma.$disconnect();
+    }
+}
+
+//post
+export const POST = async (req: Request, res: NextRequest) => {
+    try {
+        const {title, content} = await req.json();
+        await main();
+        const post  = await prisma.post.create({data:{title,content}})
+        return NextResponse.json({maessage: "success", post}, {status: 201});
     } catch (error) {
         return NextResponse.json({maessage: "Error", error}, {status: 500});
     }
